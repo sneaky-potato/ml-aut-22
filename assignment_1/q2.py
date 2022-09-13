1fd01789cc5aae364be3c1e8f75658597d1b1c82
sneaky-potato <ashwanikamal.im421@gmail.com>
2022-09-13 22:44:19 +0530

    Refactor main code to print accuracies and scores

--- assignment_1/q2.py
+++ assignment_1/q2.py
@@ -19,11 +19,12 @@ def trainTestSplit(dataSet, testRatio, shuffle=True):
 def main():
     df = pd.read_csv("cleaned_data.csv")
     train, test = trainTestSplit(df, 0.2, shuffle=True)
-    print("Starting")
     
     NB = NaiveBayes(train, test, n_folds)
+
     scores, summary = NB.fit()
 
+    print("::Naive Bayes using {} cross validation::".format(n_folds))
     for i in range(n_folds):
         print('Iteration({}) score = {}'.format(i+1,scores[i]))
 
@@ -32,6 +33,16 @@ def main():
 
     print("\nModel scores:")
     print("Train Accuracy: {}".format(train_acc))
+    print("Test Accuracy: {}".format(test_acc))
+
+
+    prior, post = NB.fit(laplace_corr=True)
+
+    print("::Naive Bayes using Laplace correction::")
+
+    test_acc = NB.get_test_accuracy_laplacian(prior, post)
+
+    print("\nModel scores:")
     print("Test Accuracy: {}".format(test_acc))
 
 if __name__ == "__main__":
