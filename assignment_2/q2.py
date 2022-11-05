7bbe13ed954562bacb0e1fe24c1c10b57621bc7a
sneaky-potato <ashwanikamal.im421@gmail.com>
2022-11-06 01:33:33 +0530

    Add driver code for q2

--- assignment_2/q2.py
+++ assignment_2/q2.py
@@ -1,8 +1,39 @@
-from BinarySVM import BinarySVM
+from Models import BinarySVM, MLP
+from data_processing import read_data, trainTestSplit, normalize
 
 def main():
-    
-    pass
+    x, y = read_data()
+
+    xTrainSet, xTestSet, yTrainSet, yTestSet = trainTestSplit(x, y)
+    xTrainSet, xTestSet = normalize(xTrainSet, xTestSet)
+
+    print("SVM::")
+    bsvmList = []
+    kernels = ['linear', 'poly', 'rbf']
+
+    best_accuracy = 0
+
+    for kernel in kernels:
+        BSVM = BinarySVM(xTrain=xTrainSet, yTrain=yTrainSet, kernel=kernel)
+        BSVM.fitModel()
+        yPrediction = BSVM.getPredict(xTest=xTestSet)
+        accuracy = BSVM.accuracy(yTestTrue=yTestSet)
+
+        print("::accuracy:: ->", accuracy)
+        bsvmList.append((BSVM, accuracy))
+
+    print("MPL")
+    mplList = []
+    hidden_layer_sizes=[(16,), (256, 16)]
+
+    for hidden_layer_size in hidden_layer_sizes:
+        MPLC = MLP(hidden_layer_sizes=hidden_layer_size, xTrain=xTrainSet, yTrain=yTrainSet)
+        MPLC.fitModel()
+        yPrediction = MPLC.getPredict(xTest=xTestSet)
+        accuracy = MPLC.accuracy(yTestTrue=yTestSet)
+
+        print("::accuracy:: ->", accuracy)
+        mplList.append((MPLC, accuracy))
 
 if __name__ == "__main__":
     main()
