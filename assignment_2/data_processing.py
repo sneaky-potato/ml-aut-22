77b7323d443109db2d6c37a6459299f369e810c7
sneaky-potato <ashwanikamal.im421@gmail.com>
2022-11-06 19:07:12 +0530

    Add forward feature selection for MLP

--- assignment_2/data_processing.py
+++ assignment_2/data_processing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from Models import MLP
 
 def read_data():
     cols_list = [
@@ -20,8 +21,6 @@ def read_data():
     ]
 
     df = pd.read_csv('wine.data', sep=',', header=None, names=cols_list)
-
-    print(df.head())
 
     x = df.iloc[:, 1:].to_numpy()
     y = df.iloc[:, 0].to_numpy()
@@ -51,3 +50,41 @@ def normalize(xTrain, xTest):
     xTestScaled = (xTest - xTrainMean) / xTrainStd
 
     return xTrainScaled, xTestScaled
+
+def forward_feature_selection(model, xTest, yTestTrue):
+    base_err = model.error(yTestTrue)
+    features = []
+    features_sample = [*range(0, len(model.xTrain[0]))]
+
+    while(True):
+        best_e = 1
+        best_feature = None
+
+        for i in features_sample:
+            xTrainTemp = model.xTrain[:, [i]]
+            xTestTemp = xTest[:, [i]]
+
+            for j in features:
+                xTrainTemp = np.concatenate([xTrainTemp, model.xTrain[:, [j]]], axis=1)
+                xTestTemp = np.concatenate([xTestTemp, xTest[:, [j]]], axis=1)
+                
+
+            mlp = MLP(model.hidden_layer_sizes, xTrainTemp, model.yTrain, model.learning_rate)
+            mlp.fitModel()
+            mlp.getPredict(xTestTemp)
+
+            e = mlp.error(yTestTrue)
+
+            if(e < best_e):
+                best_e = e
+                best_feature = i
+
+        if(best_e >= base_err): best_feature = None 
+
+        print("best feature found =>", best_feature)
+        if(best_feature is None): break
+
+        features.append(best_feature)
+        features_sample.remove(best_feature)
+
+    return features
