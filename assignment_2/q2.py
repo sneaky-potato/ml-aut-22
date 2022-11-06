01879e96af09a03036383adb96cc838682680188
sneaky-potato <ashwanikamal.im421@gmail.com>
2022-11-06 19:07:57 +0530

    Add print statements and plot in q2

--- assignment_2/q2.py
+++ assignment_2/q2.py
@@ -1,5 +1,8 @@
 from Models import BinarySVM, MLP
-from data_processing import read_data, trainTestSplit, normalize
+import numpy as np
+import statistics as st
+from data_processing import read_data, trainTestSplit, normalize, forward_feature_selection
+import matplotlib.pyplot as plt
 
 def main():
     x, y = read_data()
@@ -7,11 +10,16 @@ def main():
     xTrainSet, xTestSet, yTrainSet, yTestSet = trainTestSplit(x, y)
     xTrainSet, xTestSet = normalize(xTrainSet, xTestSet)
 
-    print("SVM::")
+    print("Number of instances in training set => {0}, and in testing set => {1}".format(len(xTrainSet), len(xTestSet)))
+    print("Feature array shape =>", xTrainSet.shape)
+    print("Label array shape =>", yTrainSet.shape)
+    print("Labels in dataset =>", np.unique(yTrainSet))
+
+    print("\nModel scores:")
+
+    print("\nSupport Vector Machine")
     bsvmList = []
     kernels = ['linear', 'poly', 'rbf']
-
-    best_accuracy = 0
 
     for kernel in kernels:
         BSVM = BinarySVM(xTrain=xTrainSet, yTrain=yTrainSet, kernel=kernel)
@@ -19,10 +27,13 @@ def main():
         yPrediction = BSVM.getPredict(xTest=xTestSet)
         accuracy = BSVM.accuracy(yTestTrue=yTestSet)
 
-        print("::accuracy:: ->", accuracy)
+        print("::{0} SVM accuracy:: =>".format(kernel), accuracy)
         bsvmList.append((BSVM, accuracy))
 
-    print("MPL")
+    bestBSVM = max(bsvmList, key=lambda tup: tup[1])[0]
+    print("Best accuracy recorded =>", bestBSVM.kernel, "SVM")
+
+    print("\nMultilayer Perceptron")
     mplList = []
     hidden_layer_sizes=[(16,), (256, 16)]
 
@@ -32,8 +43,47 @@ def main():
         yPrediction = MPLC.getPredict(xTest=xTestSet)
         accuracy = MPLC.accuracy(yTestTrue=yTestSet)
 
-        print("::accuracy:: ->", accuracy)
+        print("::{0} MLP accuracy:: =>".format(hidden_layer_size), accuracy)
         mplList.append((MPLC, accuracy))
+
+    bestMLPC = max(mplList, key=lambda tup: tup[1])[0]
+    print("Best accuracy recorded =>", bestMLPC.hidden_layer_sizes, "MLP")
+
+    learning_rate_list = [0.1, 0.01, 0.001, 0.0001, 0.00001]
+    accuracy_list =[]
+
+    print("\nMLP with different learning rates")
+
+    for lr in learning_rate_list:
+        MPLC = MLP(hidden_layer_sizes=bestMLPC.hidden_layer_sizes, xTrain=xTrainSet, yTrain=yTrainSet, learning_rate=lr)
+        MPLC.fitModel()
+        yPrediction = MPLC.getPredict(xTest=xTestSet)
+        accuracy = MPLC.accuracy(yTestTrue=yTestSet)
+
+        print("::MLP with learning rate {} accuracy:: =>".format(lr), accuracy)
+        accuracy_list.append(accuracy)
+
+    plt.xlim(0, 0.15)
+    plt.ylim(0.75, 1)
+    plt.plot(learning_rate_list, accuracy_list, '-ok')
+    point_data = [[learning_rate_list[i], accuracy_list[i]] for i in range(len(accuracy_list))]
+
+    plt.savefig("learning_rate_vs_accuracy.png", format="png")
+    plt.show()
+
+    print("\n::Running forward feature selection algorithm::")
+    features = forward_feature_selection(bestMLPC, xTestSet, yTestSet)
+
+    print("Set of features (indices of features) after forward feature selection ->", features)
+
+    # Ensemble learning
+    yEnsembledPred = np.zeros(yPrediction.shape)
+
+    for y in range(len(yEnsembledPred)):
+        yEnsembledPred[y] = st.mode([bestMLPC.yTest[y], bsvmList[1][0].yTest[y], bsvmList[2][0].yTest[y]])
+
+    ensembledAccuracy = np.mean(yEnsembledPred == yTestSet)
+    print("\nAccuracy after ensembling max voting =", ensembledAccuracy)
 
 if __name__ == "__main__":
     main()
